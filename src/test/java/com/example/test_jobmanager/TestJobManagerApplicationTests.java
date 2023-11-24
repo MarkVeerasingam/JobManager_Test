ecd4b46235b88f6b4fb09aae120f851c31c32d99
Mark Veerasingam <markveer70@gmail.com>
2023-11-24 18:21:17 +0000

    test

---
+++ src/test/java/com/example/test_jobmanager/TestJobManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_jobmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJobManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
